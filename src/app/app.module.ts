2750a5983d3f4cd76f7e8f6e0dd68cfa21b11900
goutam-naik7 <naikgautam257@gmail.com>
2021-07-10 13:06:04 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyContainerComponent } from './myContainer/myContainer.component';
+import { TopNavComponent } from './myContainer/top-nav/top-nav.component';
+import { Text1Component } from './myContainer/text1/text1.component';
+import { Text2Component } from './myContainer/text2/text2.component';
+import { HeaderComponent } from './myContainer/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainerComponent,
+    TopNavComponent,
+    Text1Component,
+    Text2Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
